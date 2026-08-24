5b6263c20418dcc3e79c99f1a213b4c81804551b
Sai Ram <sairammallidi777@gmail.com>
2026-08-24 11:40:33 +0000

    Create SaiBank application

--- app.py
+++ app.py
@@ -1,1 +1,18 @@
-print('Hello DevOps')
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Welcome to SaiBank - Version 3!"
+
+@app.route("/login")
+def login():
+    return "SaiBank Login Page"
+
+@app.route("/balance")
+def balance():
+    return "Your balance: ₹50,000"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
